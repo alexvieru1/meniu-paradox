0e76a28fb9b71083e7bc1cc0f817c891e599f538
Alexandru Vieru <vieru.alexandruu@gmail.com>
2025-06-05 20:54:24 +0300

    added to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // In either bundle we don’t need the Node canvas polyfill
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
